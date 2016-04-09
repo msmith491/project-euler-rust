2f4121881dd1a490f4668aadc8f5efd5d615a929
Matt Smith <matthew.smith491@gmail.com>
2016-04-09 09:37:42 -0700

    Finished problem_18

--- problem_18/src/main.rs
+++ problem_18/src/main.rs
@@ -1,9 +1,20 @@
 fn main() {
 
-    let s = "3
-             7 4
-             2 4 6
-             8 5 9 3"
+    let s = "75
+            95 64
+            17 47 82
+            18 35 87 10
+            20 04 82 47 65
+            19 01 23 75 03 34
+            88 02 77 73 07 63 67
+            99 65 04 28 06 16 70 92
+            41 41 26 56 83 40 80 70 33
+            41 48 72 33 47 32 37 16 94 29
+            53 71 44 65 25 43 91 52 97 51 14
+            70 11 33 28 77 73 17 78 39 68 17 57
+            91 71 52 38 17 14 91 43 58 50 27 29 48
+            63 66 04 68 89 53 67 30 73 16 69 87 40 31
+            04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"
              .split("\n")
              .map(|x| x.trim())
              .collect::<Vec<&str>>()
@@ -19,8 +30,30 @@ fn main() {
                   .collect::<Vec<usize>>())
              .collect::<Vec<Vec<usize>>>();
 
-    // println!("{:?}", s);
-    for i in 0..(s.len()) {
-        println!("{:?}", s[i]);
+    fn dpath(sb: &mut Vec<Vec<usize>>) -> usize {
+
+        loop {
+            if sb.len() == 1 {
+                break;
+            }
+            let mut sums: Vec<usize> = Vec::new();
+            let curr_row = sb.pop().unwrap();
+            let next_row = sb.pop().unwrap();
+            for (index, elem) in next_row.iter().enumerate() {
+                let mut sum = 0;
+                if curr_row.get(index).unwrap() >
+                    curr_row.get(index + 1).unwrap() {
+                    sum = curr_row.get(index).unwrap() + elem;
+                } else {
+                    sum = curr_row.get(index + 1).unwrap() + elem;
+                }
+                sums.push(sum);
+            }
+            sb.push(sums);
+        }
+        *sb.get(0).unwrap().get(0).unwrap()
     }
+
+    let mut sb = s.clone();
+    println!("{:?}", dpath(&mut sb));
 }
