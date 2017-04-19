f532afb657ebac1705f585be8b9a9519283d376e
Matt Smith <matthew.smith491@gmail.com>
2017-04-19 18:41:25 -0500

    Refactoring problem_2

--- problem_2/src/main.rs
+++ problem_2/src/main.rs
@@ -1,34 +1,17 @@
 fn main() {
 
-    fn fib(n: i32) -> i32 {
-        if n == 0 {
-            return 0;
-        } else if n == 1 {
-            return 1;
-        } else if n == 2 {
-            return 2;
-        } else {
-            return fib(n - 1) + fib(n - 2);
+    fn fib(n: usize) -> usize {
+        match n <= 3 {
+            true => n,
+            false => return fib(n - 1) + fib(n - 2),
         }
     }
 
-    let mut fib_seq = Vec::new();
-    let mut index = 1;
-    loop {
-        let current_fib = fib(index);
-        if current_fib <= 4000000 {
-            fib_seq.push(fib(index));
-        } else {
-            break;
-        }
-        index = index + 1;
-    }
-
-    let sum: i32 = fib_seq
-        .into_iter()
+    let sum = (1..)
+        .map(|x| fib(x))
+        .take_while(|&x| x <= 4000000 as usize)
         .filter(|&x| x % 2 == 0)
         .fold(0, |acc, item| acc + item);
 
     println!("{}", sum);
-        // .filter(|x: i32| x % 2 == 0)
 }
