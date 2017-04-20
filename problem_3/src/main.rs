1c4811b78e1e6c9dd7476136b4dfc473e1ea67b0
Matt Smith <matthew.smith491@gmail.com>
2017-04-19 19:02:02 -0500

    Refactoring problem_3

--- problem_3/src/main.rs
+++ problem_3/src/main.rs
@@ -1,49 +1,34 @@
-extern crate range_check;
-
-use range_check::Contains;
-
 fn main() {
 
-    fn is_prime(n: i64) -> bool {
+    fn is_prime(n: usize) -> bool {
 
-        let mut primes: Vec<i64> = vec![1, 2, 3, 5, 7];
-        if n < 1 {
-            return false;
-        } else if primes.contains(&n) {
-            return true;
-        } else if n % 2 == 0 || n % 3 == 0 {
-            return false;
-        } else {
-            let mut i = 5;
-            loop {
-                if i * i > n {
-                    break;
-                } else {
-                    if n % i == 0 || n % (i + 2) == 0 {
-                        return false;
+        let primes: Vec<usize> = vec![1, 2, 3, 5, 7];
+        match n < 1 || n % 2 == 0 || n % 3 == 0 || primes.iter().filter(|&x| *x == n).collect::<Vec<_>>().len() > 0 {
+            true => return false,
+            _ => {
+                let mut i = 5;
+                loop {
+                    if i * i > n {
+                        break;
+                    } else {
+                        if n % i == 0 || n % (i + 2) == 0 {
+                            return false;
+                        }
                     }
+                    i = i + 6;
                 }
-                i = i + 6;
-            }
+            },
         }
         true
     }
 
-    // for elem in 1..100 {
-    //     if is_prime(elem) {
-    //         println!("{}", elem);
-    //     }
-    // }
-
-    // println!("{}", is_prime(25));
 
     let num = 600851475143;
-    // let num = 131952452;
 
-    for elem in (2..((num / 2) as f64).sqrt() as i64 + 1).rev() {
-        if (elem % 2 != 0 && num % elem == 0 && is_prime(elem)) || elem == 2 {
-            println!("{}", elem);
-            break;
-        }
-    }
+    let result = (2..((num / 2) as f64).sqrt() as usize + 1)
+        .rev()
+        .filter(|&x| x % 2 != 0 && num % x == 0 && is_prime(x) || x == 2)
+        .take(1)
+        .collect::<Vec<usize>>()[0];
+    println!("{:?}", result);
 }
