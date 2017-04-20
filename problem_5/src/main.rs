7c21484ed64d9aba6df56a58ad18ccce8a42b9a9
Matt Smith <matthew.smith491@gmail.com>
2017-04-20 10:28:40 -0500

    Refactoring problem_5

--- problem_5/src/main.rs
+++ problem_5/src/main.rs
@@ -1,7 +1,9 @@
+#![feature(step_by)]
+
 fn main() {
 
-    fn even_divisible(n: i64, h: i64) -> bool {
-        for elem in 2..h {
+    fn even_divisible(n: usize, h: usize) -> bool {
+        for elem in (2..h + 1).rev() {
             if n % elem != 0 {
                 return false;
             }
@@ -9,16 +11,13 @@ fn main() {
         true
     }
 
-    for elem in 2521.. {
-        if elem % 20 != 0 {
-            continue;
-        } else if even_divisible(elem, 20) {
-            println!("{}", elem);
-            break;
-        } else {
-            if elem >= 10000000000 {
-                break;
-            }
-        }
-    }
+    let max = 20;
+
+    let result: usize = (max..)
+        .step_by(max)
+        .filter(|&x| even_divisible(x as usize, max))
+        .take(1)
+        .collect::<Vec<_>>()[0];
+
+    println!("{:?}", result);
 }
