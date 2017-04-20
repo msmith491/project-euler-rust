7c102c0a31bda23d117a713a4a75183d10a47730
Matt Smith <matthew.smith491@gmail.com>
2017-04-20 11:04:05 -0500

    Refactoring problem_7

--- problem_7/src/main.rs
+++ problem_7/src/main.rs
@@ -1,43 +1,34 @@
-extern crate range_check;
-
-use range_check::Contains;
-
 fn main() {
 
-    fn is_prime(n: i64) -> bool {
+    fn is_prime(n: usize) -> bool {
 
-        let mut primes: Vec<i64> = vec![1, 2, 3, 5, 7];
-        if n < 1 {
-            return false;
-        } else if primes.contains(&n) {
-            return true;
-        } else if n % 2 == 0 || n % 3 == 0 {
-            return false;
-        } else {
-            let mut i = 5;
-            loop {
-                if i * i > n {
-                    break;
-                } else {
-                    if n % i == 0 || n % (i + 2) == 0 {
-                        return false;
+        let primes: Vec<usize> = vec![1, 2, 3, 5, 7];
+        match n < 1 || n % 2 == 0 || n % 3 == 0 ||
+              primes.iter().filter(|&x| *x == n).collect::<Vec<_>>().len() > 0 {
+            true => return false,
+            false => {
+                let mut i = 5;
+                loop {
+                    if i * i > n {
+                        break;
+                    } else {
+                        if n % i == 0 || n % (i + 2) == 0 {
+                            return false;
+                        }
                     }
+                    i = i + 6;
                 }
-                i = i + 6;
             }
         }
         true
     }
 
-    let mut found = 5;
+    let result = (12..)
+        .filter(|&x| is_prime(x))
+        .take(10001 - 5)
+        .max()
+        .unwrap();
 
-    for num in 12.. {
-        if is_prime(num) {
-            found = found + 1;
-        }
-        if found >= 10001 {
-            println!("{}", num);
-            break;
-        }
-    }
+    println!("{:?}", result);
+
 }
